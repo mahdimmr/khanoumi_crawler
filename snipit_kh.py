8e96448952878a6f00cbccba436581948244d7d0
Mahdi Rezayi <mahdi.rezayi76@gmail.com>
2019-06-20 19:36:28 +0430

    first commit

---
+++ snipit_kh.py
@@ -0,0 +1,68 @@
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+base_url = "https://www.khanoumi.com/"
+req = requests.get(base_url)
+
+soup = BeautifulSoup(req.text, 'html.parser')
+
+
+"""
+Get the list of all <a> in main menu with option without the title
+"""
+for ul in soup.find_all("ul"):
+    if ul.attrs.get("id") == "hdr4_mnu":
+        for li in ul.find_all("li"):
+            for div in li.find_all("div"):
+                for li_2 in div.find_all("li"):
+                    for a in li_2.find_all("a"):
+                        print(a)
+
+
+"""
+Get the list of all <a> in main menu with option with the title
+"""
+for ul in soup.find_all("ul"):
+    if ul.attrs.get("id") == "hdr4_mnu":
+        for li in ul.find_all("li"):
+            for div in li.find_all("div"):
+                for li_2 in div.find_all("li"):
+                    print(li_2.find_previous("a"))
+
+
+"""
+Get the specific url that has 
+"""
+for ul in soup.find_all("ul"):
+    if ul.attrs.get("id") == "hdr4_mnu":
+        for li in ul.find_all("li"):
+            for div in li.find_all("div"):
+                for li_2 in div.find_all("li"):
+                    if li_2.find_previous("a").text == "محصولات جدید":
+                        new_product_ulr = li_2.find_previous("a").get("href")
+
+
+"""
+Get the specific url that has discount and insert into a dict 
+"""
+menu_urls = []
+discount_urls = {}
+for ul in soup.find_all("ul"):
+    if ul.attrs.get("id") == "hdr4_mnu":
+        for li in ul.find_all("li"):
+            for div in li.find_all("div"):
+                for li_2 in div.find_all("li"):
+                    menu_urls.append(li_2.find_previous("a"))
+for urls in menu_urls:
+    if urls.text == "محصولات جدید":
+        discount_urls[urls.text] = urls.get("href")
+    if urls.text == "تخفیف های روز":
+        discount_urls[urls.text] = urls.get("href")
+    if urls.text == "پرطرفدارها":
+        discount_urls[urls.text] = urls.get("href")
+    if urls.text == "شانس 10:10":
+        discount_urls[urls.text] = urls.get("href")
+    if urls.text == "بومرنگ":
+        discount_urls[urls.text] = urls.get("href")
+pprint(discount_urls)
---
+++ spider_kh.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class BlogSpider(scrapy.Spider):
+    name = 'khanoumi'
+
+    def start_requests(self):
+        urls = [
+            'https://www.khanoumi.com/newproducts#sort_3'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split('/')[-1]
+        filename = f'khanoumi_{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f"Saved file {filename}")
+
+
+
+    # for title in response.css('.post-header>h2'):
+    #     yield {'title': title.css('a ::text').get()}
+    #
+    # for next_page in response.css('a.next-posts-link'):
